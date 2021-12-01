9e9507a079805cd10225f31edc05bb4edf7e67d6
sblagojevic <sanjablagojevic96@outlook.com>
2021-12-01 14:49:52 +0100

    FAQ

--- UserManagement.MVC/Controllers/HomeController.cs
+++ UserManagement.MVC/Controllers/HomeController.cs
@@ -37,6 +37,11 @@ namespace UserManagement.MVC.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
