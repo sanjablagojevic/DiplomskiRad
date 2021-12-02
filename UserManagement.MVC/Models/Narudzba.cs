3fb76d47d6bdc1e85283a371e8bd2eb86f190776
sblagojevic <sanjablagojevic96@outlook.com>
2021-12-02 14:28:27 +0100

    Narudzbe

--- UserManagement.MVC/Models/Narudzba.cs
+++ UserManagement.MVC/Models/Narudzba.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,17 +15,25 @@ namespace UserManagement.MVC.Models
         }
 
         public int NarudzbaId { get; set; }
+        [DisplayName("Usluga")]
         public int? UslugaId { get; set; }
         public int? RadnikId { get; set; }
+        [DisplayName("Adresa naručioca")]
         public string AdresaNarudzbe { get; set; }
+        [DisplayName("Datum")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DatumNarudzbe { get; set; }
+        [DisplayName("Vrijeme početka")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
         public DateTime? VrijemePocetka { get; set; }
+        [DisplayName("Vrijeme kraja")]
         public DateTime? VrijemeKraja { get; set; }
         public bool? NarudzbaPotvrdjena { get; set; }
         public string EmailNarucioca { get; set; }
         public string BrojTelefonaNarucioca { get; set; }
 
         public virtual Radnik Radnik { get; set; }
+        [DisplayName("Usluga")]
         public virtual Usluga Usluga { get; set; }
         public virtual ICollection<Placanje> Placanje { get; set; }
     }
