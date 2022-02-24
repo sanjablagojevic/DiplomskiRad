b23700601e81a32ed7841a45d3679a1e94accaff
sblagojevic <sanjablagojevic96@outlook.com>
2022-02-24 18:16:01 +0100

    Obavezna polja

---
+++ UserManagement.MVC/Migrations/20220224171344_ObaveznaPolja.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.MVC.Migrations
+{
+    public partial class ObaveznaPolja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VrijemePocetka",
+                table: "Narudzba",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailNarucioca",
+                table: "Narudzba",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdresaNarudzbe",
+                table: "Narudzba",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "VrijemePocetka",
+                table: "Narudzba",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailNarucioca",
+                table: "Narudzba",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdresaNarudzbe",
+                table: "Narudzba",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
