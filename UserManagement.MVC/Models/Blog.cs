35df43e652becfbe341e39cc6ca0d8b887f4fc55
sblagojevic <sanjablagojevic96@outlook.com>
2022-02-25 15:09:43 +0100

    Nova tabela

---
+++ UserManagement.MVC/Models/Blog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.MVC.Models
+{
+    public class Blog
+    {
+
+        public int BlogPostId { get; set; }
+
+        [DisplayName("Author")]
+        public String UserId { get; set; }
+
+        public String Title { get; set; }
+
+        public String ShortDescription { get; set; }
+
+        public String Description { get; set; }
+
+        public byte[] BlogPostPicture { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
